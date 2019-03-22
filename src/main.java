5a3bba00aecdd60425f2437c78ecd6ed012c6801
Alexey Gladyshev <wotpricol@mail.ru>
2019-03-22 19:56:13 +0300

    Clean main

--- src/main.java
+++ src/main.java
@@ -1,30 +1,6 @@
-import client.Client;
-import server.Server;
-import thirdParty.playerTags;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class main {
     public static void main(String[] args) {
-
-        /*ExecutorService exec = Executors.newFixedThreadPool(3);
-
-        exec.execute(new Server());
-        exec.execute(new Client());
-        exec.execute(new Client());*/
-
-
-
-       /* Thread t1 = new Thread(new Server());
-        Client c1 = new Client();
-        Thread t2 = new Thread(c1);
-        Thread t3 = new Thread(new Client());
-
-        t1.start();
-        t2.start();
-        t3.start();*/
-
         System.out.println("Hello world");
     }
 }
